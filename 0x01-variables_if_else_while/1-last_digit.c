d48f033288cb91618b80e6b1d6029e076b148a8e
Juanita-Christene Adigwe <juanitacadigwe@gmail.com>
2022-12-09 04:02:29 -0800

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ int main(void)
 	if (nlast > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, nlast);
-	} else if (nlast == 0) 
+	} else if (nlast == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, nlast);
 	} else

0ef33592377d0de66ba3a477300fb14c6d56d117
Juanita-Christene Adigwe <juanitacadigwe@gmail.com>
2022-12-13 06:04:41 -0800

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,13 @@
 #include "main.h"
 /**
- * print alphabet
+ *  print_alphabet - prints alphabet
+ *
+ *  Return: always 0;
  */
 void print_alphabet(void)
 {
 	char i;
+
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar(i);

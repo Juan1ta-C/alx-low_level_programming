ec899fc08520cf14b71e54fedf766e072175e92a
Juanita-Christene Adigwe <juanitacadigwe@gmail.com>
2022-12-21 03:38:57 -0800

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strcat - appends two strings
+ * @dest: one string
+ * @src: another string
+ * Return: Pointer to dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	size_t dest_len = strlen(dest);
+	size_t i;
+
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+		dest[dest_len + i] = src[i];
+	dest[dest_len + i] = '\0';
+
+	return (dest);
+}

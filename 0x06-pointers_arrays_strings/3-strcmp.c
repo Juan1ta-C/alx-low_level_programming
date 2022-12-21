94adc77f2e11defba323f3de6bc41173143c26fc
Juanita-Christene Adigwe <juanitacadigwe@gmail.com>
2022-12-21 10:44:47 -0800

    Task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcmp - compare strings
+ * @s1: string one
+ * @s2: string two
+ * Return: 0 if equal
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, op = 0;
+
+	while (op == 0)
+	{
+		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
+			break;
+		op = *(s1 + i) - *(s2 + i);
+		i++;
+	}
+	return (0);
+}
